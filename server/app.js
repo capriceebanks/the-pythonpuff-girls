45e9b9b156d2ca8ebf11774538ac6cba7e72b67c
Caz <carolinelouisa90@gmail.com>
2022-01-31 18:45:46 +0000

    giphy working

--- server/app.js
+++ server/app.js
@@ -7,8 +7,9 @@ const axios = require('axios').default;
 
 app.use(cors());
 app.use(express.json());
+
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5501"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
