277ef69bc64203f7282db86569fe2ab661218877
Caz <carolinelouisa90@gmail.com>
2022-01-31 18:07:41 +0000

    new branch with synch updates

--- server/app.js
+++ server/app.js
@@ -13,15 +13,6 @@ app.use(function(req, res, next) {
     next();
   });
 
-app.use(cors());
-app.use(express.json());
-
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
 //check
 app.get("/", (req, res) => {
