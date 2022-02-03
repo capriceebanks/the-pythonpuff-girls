25e825a8950109890931bed258f20961131fae54
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-02-03 19:06:37 +0000

    all tests for api are passing

--- test/api.spec.js
+++ test/api.spec.js
@@ -11,19 +11,16 @@ describe('api', () => {
 
     describe('/posts', () => {
         let testData = [
-            {
-                "id": 1,
-                "message": "Made a new API!",
-                "emojis": {"heart":4, "celebrate": 3, "laugh": 1},
-                "comments": ["Wow good job!!!", "Nice!", "What does your API do??"],
-                "gifUrl": "https://media4.giphy.com/media/3o6UB3VhArvomJHtdK/giphy.gif?cid=a2b4cd36k1mdj1exmue34ziic08ykmew8x5edmxjg2nf1xbn&rid=giphy.gif&ct=g"
-            }
+            "Made a new API!",
+            ["Wow good job!!!", "Nice!", "What does your API do??"],
+            {"heart":4, "celebrate": 3, "laugh": 1},
+            "https://media4.giphy.com/media/3o6UB3VhArvomJHtdK/giphy.gif?cid=a2b4cd36k1mdj1exmue34ziic08ykmew8x5edmxjg2nf1xbn&rid=giphy.gif&ct=g"
         ];
 
-        it('GET /posts responds with status code 400', (done) => {
+        it('GET /posts responds with status code 200', (done) => {
             request(app)
             .get('/posts')
-            .expect(400, done);
+            .expect(200, done);
         });
 
         it('GET /posts/:id responds with json', (done) => {
@@ -41,13 +38,14 @@ describe('api', () => {
         it('GET /posts/comments/1 responds with correct result', (done) => {
             request(app)
             .get('/posts/comments/1')
-            .expect(testData[0].comments, done);
+            .expect(testData[1], done);
         });
 
     });
 
     describe('/gifs', () => {
-        it('GET /gifs/heart responds with correct result', () => {
+        
+        it('GET /gifs/heart responds with correct result', (done) => {
             request(app)
             .get('/gifs/heart')
             .expect('Content-Type', /json/, done);
