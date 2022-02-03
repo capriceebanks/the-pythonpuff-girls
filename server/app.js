c29e30140266541dd74356c67e225aca3e863523
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-02-03 12:19:46 +0000

    clean up comments

--- server/app.js
+++ server/app.js
@@ -8,12 +8,6 @@ const axios = require('axios').default;
 app.use(cors());
 app.use(express.json());
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5504"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-
 
 //check
 app.get("/", (req, res) => {
